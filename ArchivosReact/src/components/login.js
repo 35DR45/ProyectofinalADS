89e53cca41b482229a8438b2e569e1ba4721495f
Demian-1 <sergio_demian_ae@hotmail.com>
2022-12-16 14:27:03 -0600

    No c que hice
    ayuda

--- ArchivosReact/src/components/login.js
+++ ArchivosReact/src/components/login.js
@@ -38,18 +38,18 @@ class Login extends React.Component {
     const qId = (new URLSearchParams(window.location.search).get("val") == "true") ? true : false;
     const undiv = <div class="container w-100" style={styles} id="equis">
       <main class="form-signin w-50 m-auto">
-          <h1 class="p-5 AlignCenter">ADS Sketch RNN</h1>
-          <div class="pb-3 form-floating">            
-            <label for="User">Usuario:</label>
-            <input placeholder="Ingrese el usuario" type="text" id="User" class="form-control" />
-          </div>
-          <div class="pb-3 form-floating">
-            <label for="password">Password:</label>
-            <input placeholder="Ingrese su contraseña" type="password" id="password" class="form-control" />            
-          </div>
-          <button class="AlignCenter w-75 btn btn-lg btn-primary" onClick={() => this.validar(document.getElementById("User").value, document.getElementById("password").value)}>
-            Ingresar
-          </button>
+        <h1 class="p-5 AlignCenter">ADS Sketch RNN</h1>
+        <div class="pb-3 form-floating">
+          <label for="User">Usuario:</label>
+          <input placeholder="Ingrese el usuario" type="text" id="User" class="form-control" />
+        </div>
+        <div class="pb-3 form-floating">
+          <label for="password">Password:</label>
+          <input placeholder="Ingrese su contraseña" type="password" id="password" class="form-control" />
+        </div>
+        <button class="AlignCenter w-75 btn btn-lg btn-primary" onClick={() => this.validar(document.getElementById("User").value, document.getElementById("password").value)}>
+          Ingresar
+        </button>
       </main>
     </div>
     const esValido = (this.state.val) || qId ? <Home></Home> : undiv
--- reactBD/src/components/login.js
+++ reactBD/src/components/login.js
@@ -2,70 +2,68 @@ import React from "react";
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import $ from "jquery"
-import { browserHistory } from "react-router";
-import ReactDOM from "react-dom"
 import Home from "./home"
 
 class Login extends React.Component {
-    
-  state={
+
+  state = {
     val: false,
   }
 
-    cambiar = () =>{
-        this.setState((state)=>({
-          val:true,
-          comp: <Home></Home>
-        }))
+  cambiar = () => {
+    this.setState((state) => ({
+      val: true,
+      comp: <Home></Home>
+    }))
+  }
+  validar = (usuario, password) => {
+    var datos = {
+      User: usuario,
+      password: password
     }
-      validar=(usuario,password) =>{
-        var datos={
-            User: usuario,
-            password: password
-        }
 
-        $.get("http://localhost:8080/Proyecto/Login",datos, (resultado)=>{
-          if(resultado[0].usuario !="error"){
-            this.state.val = true;
-            this.forceUpdate();
-          }else{
-            alert("USUARIO NO REGISTRADO")
-          }
-          
-        })
-     
-    }
-    render() {
-      const qId = (new URLSearchParams(window.location.search).get("val") == "true")? true:false;
-      const undiv=  <div className = "contenedor1">
-              <div className="right">
-              <div className="loginContainer">
-              <h1 className="AlignCenter" style={{ color: "white" }}>Sketch RNN </h1>
-              <hr  style={{ borderColor : "white" }}/>
-              <Form>
-                      <Form.Group className="mb-3">
-                      <Form.Label style={{ color: "rgb(179, 179, 179)" }}>Usuario</Form.Label>
-                      <Form.Control type="text" placeholder="Ingrese el usuario" id="User"/>
-                      </Form.Group>
-                      <Form.Group className="mb-3">
-                      <Form.Label style={{ color: "rgb(179, 179, 179)" }}>Contraseña</Form.Label>
-                      <Form.Control type="password" placeholder="Ingrese su contraseña" id="password"/>
-                      <Form.Text className="text-muted">Nunca compartas tu contraseña con nadie.</Form.Text>
-                      </Form.Group>
-                      <center>
-                      <Button variant="outline-primary" size="lg" className="button" onClick={() => this.validar(document.getElementById("User").value,document.getElementById("password").value)} >Submit</Button>
-                      </center>
-                    </Form>
-                    </div>
-              </div>
-            </div>
-       const esValido = (this.state.val) || qId?<Home></Home>: undiv
-        return(
-          <div>
-            {esValido}
-            {console.log(esValido)}
-          </div>
-        )    
+    $.get("http://localhost:8080/Proyecto/Login", datos, (resultado) => {
+      if (resultado[0].usuario != "error") {
+        this.state.val = true;
+        this.forceUpdate();
+      } else {
+        alert("USUARIO NO REGISTRADO")
+      }
+
+    })
+
+  }
+  render() {
+    const qId = (new URLSearchParams(window.location.search).get("val") == "true") ? true : false;
+    const undiv = <div className="contenedor1">
+      <div className="right">
+        <div className="loginContainer">
+          <h1 className="AlignCenter" style={{ color: "white" }}>Sketch RNN </h1>
+          <hr style={{ borderColor: "white" }} />
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label style={{ color: "rgb(179, 179, 179)" }}>Usuario</Form.Label>
+              <Form.Control type="text" placeholder="Ingrese el usuario" id="User" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label style={{ color: "rgb(179, 179, 179)" }}>Contraseña</Form.Label>
+              <Form.Control type="password" placeholder="Ingrese su contraseña" id="password" />
+              <Form.Text className="text-muted">Nunca compartas tu contraseña con nadie.</Form.Text>
+            </Form.Group>
+            <center>
+              <Button variant="outline-primary" size="lg" className="button" onClick={() => this.validar(document.getElementById("User").value, document.getElementById("password").value)} >Submit</Button>
+            </center>
+          </Form>
+        </div>
+      </div>
+    </div>
+    const esValido = (this.state.val) || qId ? <Home></Home> : undiv
+    return (
+      <div>
+        {esValido}
+        {console.log(esValido)}
+      </div>
+    )
   }
 }
 export default Login; 
